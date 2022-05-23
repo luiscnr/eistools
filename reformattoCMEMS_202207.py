a80b68c43c37aee289a255fecbf08e6b80a8cd0d
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2022-05-23 09:31:36 +0200

    Test 23/05 #1

--- reformattoCMEMS_202207.py
+++ reformattoCMEMS_202207.py
@@ -69,7 +69,7 @@ def make_reformat_daily_dataset(pinfo, start_date, end_date):
         if out:
             print('***************************************************',out)
         if err:
-            errstr = err.decode('utf-0')
+            errstr = err.decode('utf-8')
             print(errstr)
             # if err.decode("utf-8").find('ncks: unrecognized option')>0:
             #     pass
