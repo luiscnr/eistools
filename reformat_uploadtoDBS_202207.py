dbf864171ef166f3d6a00a90e61a3dbb3096e818
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2022-06-28 17:52:21 +0200

    Added multi dataset option #2

--- reformat_uploadtoDBS_202207.py
+++ reformat_uploadtoDBS_202207.py
@@ -7,9 +7,9 @@ import uploadtoDBS_202207 as upload
 
 import check_202207 as checkf
 
-parser = argparse.ArgumentParser(description='Upload 2DBS')
+parser = argparse.ArgumentParser(description='Reformat and upload 2DBS')
 parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
-parser.add_argument("-check","--check_param", help="Check params mode.", action="store_true")
+#parser.add_argument('-check', "--check_param", help="Check params mode.", action="store_true")
 parser.add_argument("-m", "--mode", help="Mode.", type=str, required=True, choices=['NRT', 'DT', 'MY'])
 parser.add_argument("-r", "--region", help="Region.", type=str, choices=['BAL', 'MED', 'BLK'])
 parser.add_argument("-l", "--level", help="Level.", type=str, choices=['l3', 'l4'])
@@ -37,25 +37,26 @@ def main():
     do_multiple_datasets = False
 
     if args.mode and args.region and args.level:
-        #pinfo.set_dataset_info_fromparam(args.mode, args.region, args.level, args.dataset_type, args.sensor)
+        # pinfo.set_dataset_info_fromparam(args.mode, args.region, args.level, args.dataset_type, args.sensor)
         dataset_type = None
         sensor = None
         if args.dataset_type:
             dataset_type = args.dataset_type
         if args.sensor:
             sensor = args.sensor
-        name_products, name_datasets = pinfo.get_list_datasets_params(args.mode, args.region, args.level, dataset_type, sensor)
+        name_products, name_datasets = pinfo.get_list_datasets_params(args.mode, args.region, args.level, dataset_type,
+                                                                      sensor)
         n_datasets = len(name_products)
 
     elif args.name_product and args.name_dataset:
         name_products.append(args.name_product)
         name_datasets.append(args.name_dataset)
         n_datasets = 1
-        #pinfo.set_dataset_info(args.name_product, args.name_dataset)
+        # pinfo.set_dataset_info(args.name_product, args.name_dataset)
     elif args.name_product and not args.name_dataset:
         name_products, name_datasets = pinfo.get_list_datasets(args.name_product)
         n_datasets = len(name_products)
-        #pinfo.set_product_info(args.name_product)
+        # pinfo.set_product_info(args.name_product)
         do_multiple_datasets = True
 
     if n_datasets == 0:
@@ -79,10 +80,12 @@ def main():
     start_date = get_date_from_param(start_date_p)
     end_date = get_date_from_param(end_date_p)
     if start_date is None:
-        print(f'[ERROR] Start date {start_date_p} is not in the correct format. It should be YYYY-mm-dd or integer (relative days')
+        print(
+            f'[ERROR] Start date {start_date_p} is not in the correct format. It should be YYYY-mm-dd or integer (relative days')
         return
     if end_date is None:
-        print(f'[ERROR] End date {end_date_p} is not in the correct format. It should be YYYY-mm-dd or integer (relative days')
+        print(
+            f'[ERROR] End date {end_date_p} is not in the correct format. It should be YYYY-mm-dd or integer (relative days')
         return
     if start_date > end_date:
         print(f'[ERROR] End date should be greater or equal than start date')
@@ -90,16 +93,16 @@ def main():
     if args.verbose:
         print(f'[INFO] Start date: {start_date} End date: {end_date}')
 
-    if args.check_param:
-        return
+    # if args.check_param:
+    #     return
 
-    #if args.start_date and args.end_date and not do_multiple_datasets:
+    # if args.start_date and args.end_date and not do_multiple_datasets:
     for idataset in range(n_datasets):
-        #start_date = dt.strptime(args.start_date, '%Y-%m-%d')
-        #end_date = dt.strptime(args.end_date, '%Y-%m-%d')
-        pinfo.set_dataset_info(name_products[idataset],name_datasets[idataset])
+        # start_date = dt.strptime(args.start_date, '%Y-%m-%d')
+        # end_date = dt.strptime(args.end_date, '%Y-%m-%d')
+        pinfo.set_dataset_info(name_products[idataset], name_datasets[idataset])
         if args.verbose:
-            print
+            print(f'[INFO] Working with dataset: {name_products[idataset]}/{name_datasets[idataset]}')
         if pinfo.dinfo['frequency'] == 'd':
             pinfo.MODE = 'REFORMAT'
             if args.verbose:
@@ -170,11 +173,12 @@ def main():
             # reformat.make_reformat_monthly_dataset(pinfo, start_date, end_date, args.verbose)
             # file_list = pinfo.get_list_file_path_orig_monthly(start_date, end_date)
 
+
 def get_date_from_param(dateparam):
     datefin = None
     try:
         ndays = int(dateparam)
-        datefin = dt.now().replace(hour=12,minute=0,second=0,microsecond=0)+timedelta(days=ndays)
+        datefin = dt.now().replace(hour=12, minute=0, second=0, microsecond=0) + timedelta(days=ndays)
     except:
         try:
             datefin = dt.strptime(dateparam, '%Y-%m-%d')
@@ -182,6 +186,7 @@ def get_date_from_param(dateparam):
             pass
 
     return datefin
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
