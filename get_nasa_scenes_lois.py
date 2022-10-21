1f23f9ff4a3db46ea0f008ddfa588e6a403a89c7
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2022-10-21 12:17:03 +0200

    Added Nasa Download

---
+++ get_nasa_scenes_lois.py
@@ -0,0 +1,32 @@
+from nasa_download import NASA_DOWNLOAD
+from datetime import datetime as dt
+import argparse
+
+parser = argparse.ArgumentParser(description='NASA Get Scenes. Patch to retrieve DT scenes')
+parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
+parser.add_argument("-sen", "--sensor", help="Specify sensor: VIIRS, VIIRSJ, AQUA", required=True)
+parser.add_argument("-d", "--date", help="Specify a date in yyyymmdd format",required=True)
+parser.add_argument("-R", "--Region", help="specify the Region Label of the area: BlackSea or Mediterranean", required=False)
+args = parser.parse_args()
+
+def main():
+
+    try:
+        date_here = dt.strptime(args.date,'%Y%m%d')
+        sensor = args.sensor
+        if args.Region=='BlackSea':
+            region = 'BS'
+        elif args.Region=='Mediterranean':
+            region = 'MED'
+        #date_here = dt(2022,9,30)
+    except:
+        print('[ERROR] Error parsing arguments')
+        exit(1)
+
+    ndownload = NASA_DOWNLOAD()
+    ndownload.get_list_files(date_here,sensor,region,'DT')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
---
+++ nasa_download.py
@@ -0,0 +1,102 @@
+from datetime import datetime as dt
+import os, re
+import urllib.request
+
+
+class NASA_DOWNLOAD:
+    def __init__(self):
+        self.direct_access_base_url = 'https://oceandata.sci.gsfc.nasa.gov/directdataaccess/Level-2/'
+        self.sensors = {
+            'AQUA': {
+                'direct_access_folder': 'Aqua-MODIS'
+            },
+            'VIIRSJ': {
+                'direct_access_folder': 'NOAA20-VIIRS',
+                'nrt_wce': "JPSS1_VIIRS.DATET\d*\.L2\.OC\.NRT\.nc",
+                'dt_wce': "JPSS1_VIIRS.DATET\d*\.L2\.OC\.nc",
+                "nrt_cnr_server_dir": "/store3/OC/VIIRSJ/daily",
+                "nrt_cnr_server_wce": "DATE_VIIRSJ_NRT_REGION_oc_proc_L2_",
+                "prefix": "JPSS1_VIIRS.DATE"
+
+            },
+            'VIIRS': {
+                'direct_access_folder': 'SNPP-VIIRS',
+                'nrt_wce': "SNPP_VIIRS.DATET\d*\.L2\.OC\.NRT\.nc",
+                'dt_wce': "SNPP_VIIRS.DATET\d*\.L2\.OC\.nc",
+                "nrt_cnr_server_dir": "/store3/OC/VIIRSJ/daily",
+                "nrt_cnr_server_wce": "DATE_VIIRS_NRT_REGION_oc_proc_L2_",
+                "prefix": "SNPP_VIIRS.DATE"
+            }
+        }
+
+    def get_url_date(self, sensor, date_here):
+        year_str = date_here.strftime('%Y')
+        jday_str = date_here.strftime('%j')
+        url = os.path.join(self.direct_access_base_url, self.sensors[sensor]['direct_access_folder'], year_str,
+                           jday_str)
+        return url
+
+    def get_wce_nrt_cnr_server(self, sensor, date_here, region):
+        wce = self.sensors[sensor]['nrt_cnr_server_wce']
+        wce = wce.replace('DATE', date_here.strftime('%Y%j'))
+        wce = wce.replace('REGION', region.upper())
+        return wce
+
+    def get_folder_nrt_cnr_server(self, sensor, date_here):
+        path_base = self.sensors[sensor]['nrt_cnr_server_dir']
+        folder = os.path.join(path_base, date_here.strftime('%Y'), date_here.strftime('%j'))
+        return folder
+
+    def get_expected_prefix_files_from_nrt_cnr_server(self, sensor, date_here, region):
+        path_log = self.get_folder_nrt_cnr_server(sensor, date_here)
+        wce = self.get_wce_nrt_cnr_server(sensor, date_here, region)
+        lprefix = []
+        for name in os.listdir(path_log):
+            if name.startswith(wce):
+                date_str_file = name.split('_')[7][1:].split('.')[0]
+                date_file = dt.strptime(date_str_file, '%Y%j%H%M%S')
+                str_date_prefix = date_file.strftime('%Y%m%dT%H%M')
+                prefix = self.sensors[sensor]['prefix']
+                prefix = prefix.replace('DATE', str_date_prefix)
+                lprefix.append(prefix)
+        if len(lprefix) == 0:
+            return None
+        return lprefix
+
+    def get_list_files(self, date_here, sensor, region, mode):
+
+        datestr = date_here.strftime('%Y%m%d')
+        url_date = self.get_url_date(sensor, date_here)
+
+        request = urllib.request.Request(url_date)
+        response = urllib.request.urlopen(request)
+        page = response.read()
+        page = page.decode('utf-8')
+        if mode.upper() == 'NRT':
+            wce = self.sensors[sensor]['nrt_wce']
+            suffix = 'L2.OC.NRT.nc'
+        elif mode.upper() == 'DT':
+            wce = self.sensors[sensor]['dt_wce']
+            suffix = 'L2.OC.nc'
+        wce = wce.replace('DATE', datestr)
+
+        all_scenes = list(re.findall(wce, page))
+        prefixes = self.get_expected_prefix_files_from_nrt_cnr_server(sensor, date_here, region)
+
+        scenes = []
+        for scene in all_scenes:
+            for prefix in prefixes:
+                if scene.startswith(prefix) and scene.endswith(suffix):
+                    scenes.append(scene)
+                    break
+        scenes = list(set(scenes))
+
+        print(' '.join(scenes))
+
+        # scenes = []
+        # for scene in all_scenes:
+        #     #print(scene)
+        #     if scene.endswith(suffix):
+        #         scenes.append(scene)
+
+        # print(scenes)
