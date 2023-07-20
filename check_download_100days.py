8b670d22cf1b8f2a5b814f8a1a424a4706a62f45
luiscnr <lois@LAPTOP-SU04997U.localdomain>
2023-07-20 09:47:33 +0200

    Modified checking 100 g

--- check_download_100days.py
+++ check_download_100days.py
@@ -12,8 +12,8 @@ args = parser.parse_args()
 
 
 def check_next_day():
-    # sdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    sdir = '/mnt/c/DATA_LUIS/OCTAC_WORK/CC0C-591-_100days/'
+    sdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    #sdir = '/mnt/c/DATA_LUIS/OCTAC_WORK/CC0C-591-_100days/'
     file_json = os.path.join(sdir, 'info100days.json')
     if not os.path.exists(file_json):
         print(f'NONE;File: {file_json} does not exist')
@@ -88,7 +88,8 @@ def check_sensor_dates(jdict):
 
 
 def check_mail():
-    sdir = '/mnt/c/DATA_LUIS/OCTAC_WORK/CC0C-591-_100days/'
+    sdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+    #sdir = '/mnt/c/DATA_LUIS/OCTAC_WORK/CC0C-591-_100days/'
     file_json = os.path.join(sdir, 'info100days.json')
     if not os.path.exists(file_json):
         print(f'[ERROR] File: {file_json} does not exist')
