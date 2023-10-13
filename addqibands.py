78c75d8306e28c53de968d333e9634ddf4b8dabf
luiscnr <lois@LAPTOP-SU04997U>
2023-10-13 08:55:40 +0200

    Added add empty qi band option

--- addqibands.py
+++ addqibands.py
@@ -1,4 +1,6 @@
 import argparse
+import os.path
+
 from product_info import ProductInfo
 from datetime import datetime as dt
 from datetime import timedelta
@@ -32,8 +34,87 @@ def main():
             print(f'[WARNING] File for date {date_work} is not available. Skipping...')
             date_work = date_work + timedelta(hours=24)
             continue
+        add_empty_qi_bands(file_orig)
 
         date_work = date_work + timedelta(hours=24)
+
+def add_empty_qi_bands(file_in):
+    if args.verbose:
+        print(f'[INF0] Add qi bands for file: {file_in}')
+    from netCDF4 import Dataset
+    import numpy as np
+    vars_no_qi = ['lat','lon','stereographic','time','x','y']
+    file_out = os.path.join(os.path.dirname(file_in),f'{os.path.basename(file_in)}.tmp.nc')
+
+    input_dataset = Dataset(file_in)
+    addqi = False
+    for name, variable in input_dataset.variables.items():
+        if name not in vars_no_qi and not name.startswith('QI'):
+            newname = f'QI_{name}'
+            if newname in input_dataset.variables:
+                print(f'[INFO] {newname} already exist.')
+            else:
+                addqi = True
+    if not addqi:
+        print(f'[INFO] QI bands already exists. Skipping...')
+        input_dataset.close()
+        return
+
+
+
+    ncout = Dataset(file_out, 'w', format='NETCDF4')
+
+    # copy global attributes all at once via dictionary
+    ncout.setncatts(input_dataset.__dict__)
+
+    # copy dimensions
+    for name, dimension in input_dataset.dimensions.items():
+        ncout.createDimension(
+            name, (len(dimension) if not dimension.isunlimited() else None))
+
+    for name, variable in input_dataset.variables.items():
+        fill_value = None
+        if '_FillValue' in list(variable.ncattrs()):
+            fill_value = variable._FillValue
+
+        ncout.createVariable(name, variable.datatype, variable.dimensions, fill_value=fill_value, zlib=True,
+                             shuffle=True, complevel=6)
+
+        # copy variable attributes all at once via dictionary
+        ncout[name].setncatts(input_dataset[name].__dict__)
+
+
+        # copy data
+        if name.startswith('RRS'):
+            array = np.array(input_dataset.variables[name])
+            ncout[name].valid_max=1.0
+            ncout[name][:] = array[:]
+        else:
+            ncout[name][:] = input_dataset[name][:]
+
+        if name not in vars_no_qi:
+            newname = f'QI_{name}'
+            print(f'[INFO]--> Adding band: {newname}')
+            varqi = ncout.createVariable(newname,variable.datatype,variable.dimensions,fill_value=fill_value,zlib=True,shuffle=True,complevel=6)
+            #print(ncout[name].long_name)
+            lname = f'Quality Index for  {input_dataset[name].long_name.lower()}'
+            if lname.find('rrs')>0:
+                lname = lname.replace('rrs','')
+                lname = lname + ' nm'
+
+            varqi.long_name = lname
+            varqi.comment = f'QI=(DailyData-ClimatologyWeightedMean)/ClimatologyWeightedStandardDeviation'
+            varqi.type = 'surface'
+            varqi.units = '1'
+            varqi.missing_value = -999.0
+            varqi.valid_min = -5.0
+            varqi.valid_max = 5.0
+
+
+    ncout.close()
+    input_dataset.close()
+
+    os.rename(file_out,file_in)
 
 
 ##DATES SELECTION
