a216d6c59e9fb966e558b1dc5ce9556e8f6be791
luiscnr <lois@LAPTOP-SU04997U>
2023-11-13 10:42:28 +0100

    Preparing olci reprocessing via ftp

--- eisutils.py
+++ eisutils.py
@@ -8,7 +8,8 @@ import os
 
 parser = argparse.ArgumentParser(description='Check upload')
 parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
-parser.add_argument("-m", "--mode", help="Mode.", type=str, required=True, choices=['COPYAQUA','FTPDOWNLOAD'])
+parser.add_argument("-m", "--mode", help="Mode.", type=str, required=True,
+                    choices=['COPYAQUA', 'CHECKFTPCONTENTS', 'CHECKGRANULES','CHECKSOURCES'])
 parser.add_argument("-p", "--path", help="Path for FTPDOWNLOAD")
 parser.add_argument("-sd", "--start_date", help="Start date (yyyy-mm-dd)")
 parser.add_argument("-ed", "--end_date", help="Start date (yyyy-mm-dd)")
@@ -19,50 +20,172 @@ args = parser.parse_args()
 def main():
     if args.mode == 'COPYAQUA':
         copy_aqua()
-    if args.mode == 'FTPDOWNLOAD':
-        do_ftp_download()
+    if args.mode == 'CHECKFTPCONTENTS':
+        check_ftp_contents()
+    if args.mode == 'CHECKGRANULES':
+        check_granules()
+    if args.mode == 'CHECKSOURCES':
+        check_sources()
+
+def check_sources():
+    dir_orig = '/store/COP2-OC-TAC/OLCI_FTP_EUMETSAT'
+    #arc
+    dir_sources = '/store/COP2-OC-TAC/arc/sources'
+    check_sources_impl(dir_orig,dir_sources,'arg')
+
+def check_sources_impl(dir_orig,dir_sources,region):
+    file_orig = os.path.join(dir_orig,f'new_granules_{region}.csv')
+    list_granules = {}
+    list_granules_date = []
+    date_prev_str  = None
+    f1 = open(file_orig,'r')
+    for line in f1:
+        jjj = line.split(';')[0].strip()
+        granule = line.split(';')[1].strip()
+        date_here_str = f'2023{jjj}'
+        if date_prev_str is None:
+            list_granules_date = [granule]
+            date_prev_str = date_here_str
+        else:
+            if date_here_str==date_prev_str:
+                list_granules_date.append(granule)
+            else:
+                date_here = dt.strptime(date_prev_str, '%Y%j')
+                if region=='arc':
+                    folder_date = os.path.join(dir_sources,date_here.strftime('%Y%m%d'))
+                else:
+                    folder_date = os.path.join(dir_sources,'2023',date_here.strftime('%j'))
+                list_granules[date_prev_str] = {
+                    'list':list_granules,
+                    'folder': folder_date
+                }
+                list_granules_date = [granule]
+                date_prev_str = date_here_str
+    date_here = dt.strptime(date_prev_str, '%Y%j')
+    if region == 'arc':
+        folder_date = os.path.join(dir_sources, date_here.strftime('%Y%m%d'))
+    else:
+        folder_date = os.path.join(dir_sources, '2023', date_here.strftime('%j'))
+    list_granules[date_prev_str] = {
+        'list': list_granules_date,
+        'folder': folder_date
+    }
+    f1.close()
+
+    file_remove = os.path.join(dir_orig,f'ToRemove_{region}.sh')
+    fw = open(file_remove,'w')
+
+    for date_ref in list_granules:
+        list = list_granules[date_ref]['list']
+        folder = list_granules[date_ref]['folder']
+        for name in os.listdir(folder):
+            b = check_grunule_in_list(name,list)
+            if b:
+                fout = os.path.join(folder,name)
+                fw.write(f'mv {fout} /store/COP2-OC-TAC/OLCI_FTP_EUMETSAT/2023')
+    fw.close()
+
+def check_granules():
+
+    ##WFR LIST
+    #file_new_granules = '/mnt/c/DATA_LUIS/TEMPORAL/2023/S3_WFR_Granules.csv'
+    #check_granules_region('arc',file_new_granules)
+    #check_granules_region('med',file_new_granules)
+    #check_granules_region('blk', file_new_granules)
+
+    ##WRR LIST
+    #file_new_granules = '/mnt/c/DATA_LUIS/TEMPORAL/2023/S3_WRR_Granules.csv'
+    #check_granules_region('med_rr', file_new_granules)
+    #check_granules_region('blk_rr', file_new_granules)
+
+    ##ERR LIST
+    file_new_granules = '/mnt/c/DATA_LUIS/TEMPORAL/2023/S3_EFR_Granules.csv'
+    check_granules_region('bal', file_new_granules)
 
 
-def do_ftp_download():
+def check_granules_region(region,file_new_granules):
+    input_path = '/mnt/c/DATA_LUIS/TEMPORAL/2023'
+    import pandas as pd
+    output_file = f'/mnt/c/DATA_LUIS/TEMPORAL/new_granules_{region}.csv'
+    f1 = open(output_file, 'w')
+    f1.write('Day;Granule')
+    dset = pd.read_csv(file_new_granules, sep=';')
+    date_here_prev = None
+    list_granules = []
+    for index, row in dset.iterrows():
+        day = row['Day']
+        granule = row['Granule']
+        date_here_str = dt.strptime(f'2023{day}', '%Y%j').strftime('%Y%m%d')
+        if date_here_prev is None or date_here_str != date_here_prev:
+            flist = os.path.join(input_path, f'eum_filelist_{region}_{date_here_str}.txt')
+            list_granules = []
+            if os.path.exists(flist):
+                fg = open(flist)
+                for g in fg:
+                    list_granules.append(g)
+                fg.close()
+            date_here_prev = date_here_str
+        if len(list_granules) > 0:
+            b = check_grunule_in_list(granule, list_granules)
+            if b:
+                f1.write('\n')
+                f1.write(f'{day};{granule}')
+    f1.close()
+
+
+def check_grunule_in_list(granule, list_granules):
+    format = '%Y%m%dT%H%M%S'
+    start_date_granule = dt.strptime(granule.split('_')[7], format)
+    end_date_granule = dt.strptime(granule.split('_')[8], format)
+    platform = granule.split('_')[0]
+    for g in list_granules:
+        if g.startswith(platform):
+            start_date_g = dt.strptime(g.split('_')[7], format)
+            end_date_g = dt.strptime(g.split('_')[8], format)
+            overlap = compute_overlap(start_date_granule, end_date_granule, start_date_g, end_date_g)
+            if overlap>0.99:
+               return True
+
+    return False
+
+
+def compute_overlap(sd, ed, sdcheck, edcheck):
+    overlap = 0
+    total_s = (ed - sd).total_seconds()
+    if edcheck < sd:
+        return overlap
+    if sdcheck > ed:
+        return overlap
+    if sdcheck <= sd <= edcheck:
+        overlap = (edcheck - sd).total_seconds() / total_s
+    if sdcheck >= sd and edcheck <= ed:
+        overlap = (edcheck - sdcheck).total_seconds() / total_s
+    if sdcheck<sd and edcheck>ed:
+        overlap = 1
+    if sdcheck <= ed <= edcheck:
+        overlap = (ed - sdcheck).total_seconds() / total_s
+    return overlap
+
+
+def check_ftp_contents():
     from ftplib import FTP
-    ftp_orig = FTP('cdrftp.eumetsat.int','cdr_ro','LNnh73tfAavaC3YmqXfzafVn')
-    #rpath = '/cdrftp/olci_l1l2_2023/S3A/OL_1_EFR/2023'
-    rpath = args.path
-    #output_path = '/mnt/c/DATA_LUIS/TEMPORAL/2023'
-    output_path = '/store/COP2-OC-TAC/OLCI_TEMP/2023'
-
-    for jjj in range(108,132):
+    ftp_orig = FTP('cdrftp.eumetsat.int', 'cdr_ro', 'LNnh73tfAavaC3YmqXfzafVn')
+    rpath = '/cdrftp/olci_l1l2_2023/S3B/OL_2_WRR/2023'
+    output_path = '/mnt/c/DATA_LUIS/TEMPORAL/2023/S3B_WRR_Granules.csv'
+    f1 = open(output_path, 'w')
+    f1.write('Day;Granule')
+    for jjj in range(108, 132):
         ftp_orig.cwd(rpath)
         jjjs = str(jjj)
         if jjjs in ftp_orig.nlst():
             print(f'[INFO] Day: {jjjs}')
             jjj_path = f'{rpath}/{jjjs}'
-            output_path_day = os.path.join(output_path,jjjs)
-            if not os.path.isdir(output_path_day):
-                os.mkdir(output_path_day)
+
             ftp_orig.cwd(jjj_path)
             for name in ftp_orig.nlst():
-                print(f'[INFO] Started download for folder: {name}')
-                image_original_path = f'{jjj_path}/{name}'
-                image_output_path = os.path.join(output_path_day,name)
-                if not os.path.exists(image_output_path):
-                    os.mkdir(image_output_path)
-                b = ftp_download_folder(ftp_orig,image_original_path,image_output_path)
-                if not b:
-                    print(f'[ERROR] Error download the folder {name}')
-                else:
-                    print(f'[INFO] Download complete for folder: {name}')
-                ftp_orig.cwd(jjj_path)
-def ftp_download_folder(ftp,remote_path,output_path):
-    ftp.cwd(remote_path)
-    for name in ftp.nlst():
-        remote_file_name = os.path.join(remote_path,name)
-        output_file = os.path.join(output_path,name)
-        try:
-            ftp.retrbinary("RETR " + remote_file_name, open(output_file, 'wb').write)
-        except:
-            return False
-    return True
+                f1.write('\n')
+                f1.write(f'{jjjs};{name}')
+    f1.close()
 
 
 def copy_aqua():
