353d64c0ff58afbf7bf1e0d705465afb4427a126
luiscnr <lois@LAPTOP-SU04997U>
2024-11-08 11:17:01 +0100

    Added tool to extract PDU for OLCI using CSVr files and the extract folder

--- identify_pdu_extracts.py
+++ identify_pdu_extracts.py
@@ -2,12 +2,14 @@ import argparse,os
 import numpy as np
 import pandas as pd
 from datetime import datetime as dt
+from datetime import timedelta
 
 parser = argparse.ArgumentParser(description='Identifity PDU from CSVr files')
 parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
 parser.add_argument("-m", "--mode", help="Mode.", type=str, required=True,choices=['OLCI'])
 parser.add_argument("-i", "--input_path", help="Input csv path with: OLCI: Sat_Time (as YYYY-mm-dd HH:MM), Platform and Site",required=True)
 parser.add_argument("-e", "--folder_extracts",help="Extracts folders",required=True)
+parser.add_argument("-s", "--folder_sources",help="Main source folder")
 args = parser.parse_args()
 
 
@@ -27,6 +29,14 @@ def make_olci():
     except:
         print(f'[ERROR] {input_file} is not a valid csv file')
         return
+
+    source_folder = None
+    if args.folder_sources:
+        if os.path.isdir(args.folder_sources):
+            source_folder = args.folder_sources
+        else:
+            print(f'[WARNING] {args.folder_sources} in not a valid directory and it could not be used')
+
 
     col_names =  ['Sat_Time','Platform','Site']
     for col_name in col_names:
@@ -60,14 +70,50 @@ def make_olci():
         sat_time = sat_times[idx]
         site = sites[idx]
         platform = platforms[idx]
+        sat_time_s = dt.strptime(sat_time,'%Y-%m-%d %H:%M')
+        sat_time_s_more = sat_time_s + timedelta(minutes=1)
+        sat_time_s_less = sat_time_s - timedelta(minutes=1)
         ref = f'S3{platform}_{sat_time}'
+        ref_more = f'S3{platform}_{sat_time_s_more.strftime("%Y-%m-%d %H:%M")}'
+        ref_less = f'S3{platform}_{sat_time_s_less.strftime("%Y-%m-%d %H:%M")}'
+        product = None
         if ref in extract_list[site].keys():
-            pdu[idx] = extract_list[site][ref]
-            print(f'[INFO] -> {site}_{ref}->{pdu[idx]}')
+            product = extract_list[site][ref]
+            print(f'[INFO] -> {site}_{ref}->{product}')
+        elif ref_more in extract_list[site].keys():
+            product = extract_list[site][ref_more]
+            print(f'[INFO] -> {site}_{ref}->{product}')
+        elif ref_less in extract_list[site].keys():
+            product = extract_list[site][ref_less]
+            print(f'[INFO] -> {site}_{ref}->{product}')
+        if product is not None:
+            if product.find('_trim_')>0 and source_folder is not None:
+                start_product = dt.strptime(product.split('_')[7],'%Y%m%dT%H%M%S')
+                end_product =  dt.strptime(product.split('_')[8],'%Y%m%dT%H%M%S')
+                product_complete = search_product_complete(source_folder,name[0:12],start_product,end_product)
+                if product_complete is not None: product = product_complete
+            pdu[idx] = product
 
     df['PRODUCT'] = pdu
     df.to_csv(input_file,sep=';')
     print(f'[INFO] Completed')
+
+def search_product_complete(source_folder,prename,start_product,end_product):
+    yyyy = start_product.strftime('%Y')
+    jjj = start_product.strftime('%')
+    path_source = os.path.join(source_folder,yyyy,jjj)
+    product_complete = None
+    if os.path.isdir(path_source):
+        for name in os.listdir(path_source):
+            if name.startswith(prename):
+                start_here = dt.strptime(name.split('_')[7], '%Y%m%dT%H%M%S')
+                end_here = dt.strptime(name.split('_')[8], '%Y%m%dT%H%M%S')
+                if start_here<=start_product and end_here>=end_product:
+                    product_complete = name[:-4]
+                    break
+    return product_complete
+
+
 
 
 # Press the green button in the gutter to run the script.
