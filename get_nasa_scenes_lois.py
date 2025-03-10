5016ce06a9f97f9b4e14e3f8632372a75fee4588
luiscnr <lois@LAPTOP-SU04997U>
2025-03-10 18:35:24 +0100

    Update 10-03

--- get_nasa_scenes_lois.py
+++ get_nasa_scenes_lois.py
@@ -1,22 +1,31 @@
-# from nasa_download import NASA_DOWNLOAD
-# from datetime import datetime as dt
-# import argparse
+import argparse
 
-# def main():
-#     try:
-#         date_here = dt.strptime(args.date,'%Y%m%d')
-#         sensor = args.sensor
-#         if args.Region=='BlackSea':
-#             region = 'BS'
-#         elif args.Region=='Mediterranean':
-#             region = 'MED'
-#         #date_here = dt(2022,9,30)
-#     except:
-#         print('[ERROR] Error parsing arguments')
-#         exit(1)
-#
-#     ndownload = NASA_DOWNLOAD()
-#     ndownload.get_list_files(date_here,sensor,region,'DT')
+parser = argparse.ArgumentParser(description='NASA Get Scenes. Patch to retrieve DT scenes')
+parser.add_argument("-m", "--mode", help="Mode", choices=["LIST", "DOWNLOAD", "NRT_TO_DT", "TEST"])
+parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
+parser.add_argument("-sen", "--sensor",
+                    help="Specify sensor: VIIRS, VIIRSJ, AQUA, PACE_AOP (options in the configuration file)",
+                    required=True)
+parser.add_argument("-d", "--date",
+                    help="Specify a date in yyyymmdd format, or a date list file if option -list_dates is activated",
+                    required=True
+                    )
+parser.add_argument("-R", "--Region",
+                    help="Specify the Region Label of the area: BlackSea or Mediterranean, for searching using the browser",
+                    required=False)
+parser.add_argument("-lat_p", "--lat_point", help="Station latitude")
+parser.add_argument("-lon_p", "--lon_point", help="Stationg longitude")
+parser.add_argument("-site", "--site_name", help="Pre-defined sites in configuration file")
+parser.add_argument("-ed", "--end_date",
+                    help="End date for DOWNLOAD option in yyyymmdd format (=date if this option is ignored")
+parser.add_argument("-list_dates", "--use_list_dates",
+                    help="If this option is given, -d shoud be a text file containing a data list in format yyyy-mm-dd",
+                    action="store_true")
+parser.add_argument("-nrt", "--use_nrt", help="Download/list NRT files", action="store_true")
+parser.add_argument("-o", "--path_out", help="Path out")
+parser.add_argument("-ow", "--overwrite", help="Overwrite download files")
+args = parser.parse_args()
+
 
 def get_lat_lon_from_site_name(site):
     sites = {
@@ -48,170 +57,146 @@ def get_lat_lon_from_site_name(site):
     return latP, lonP
 
 
-def download_date(ndownload, sensor, region, lat_point, lon_point, date_here):
+def download_date(ndownload, sensor, site_name, lat_point, lon_point, date_here):
+    import os
+    import obdaac_download as od
+
     path_out = ndownload.get_path_orig(sensor, date_here)
     is_dt = True if not args.use_nrt else False
-    if region is not None:
-        list = ndownload.get_list_files(date_here, sensor, region, 'DT')
+    if site_name is not None:
+        list = ndownload.getscenes_by_site_EarthData_API(sensor,date_here,site_name,is_dt)
     elif lat_point is not None and lon_point is not None:
-        list = ndownload.getscenes_by_point_EarthData_API(sensor, date_here, lat_point, lon_point,is_dt)
+        list = ndownload.getscenes_by_point_EarthData_API(sensor, date_here, lat_point, lon_point, is_dt)
 
     if len(list) > 0:
         print(f'[INFO] {len(list)} granules identified for date {date_here}')
         for granule in list:
             print(f'[INFO] Downloading granule: {granule}')
-            file_granule = os.path.join(path_out,granule)
+            file_granule = os.path.join(path_out, granule)
             if os.path.exists(file_granule) and not args.overwrite:
                 print(f'[WARNING] Ouput file: {file_granule} already exists. Skipping....')
                 return
 
             url = ndownload.get_url_download(granule)
-            od.do_download(url, path_out, appkey)
+            od.do_download(url, path_out, ndownload.apikey)
     else:
         print(f'[INFO] No granules were found to be downloaded for date: {date_here}')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    import argparse
+def make_test():
+    from nasa_download import NASA_DOWNLOAD
+    ndownload = NASA_DOWNLOAD()
+    from datetime import datetime as dt
+    from datetime import timedelta
+    fout = '/mnt/c/DATA_LUIS/OCTAC_WORK/BAL_EVOLUTION_202411/AQUA_2019.csv'
+    fw = open(fout, 'w')
+    work_date = dt(2019, 6, 1)
+    end_date = dt(2019, 9, 30)
+    geo_limits = [53, 65, 12, 31]
+    fw.write('Date;NImages')
+    while work_date <= end_date:
+        print('DATE: ', work_date)
+        # work_date_str = work_date.strftime('%Y%m%d')
+        list = ndownload.getscenes_by_region_EarthData_API('AQUA', work_date, geo_limits, True)
+        fw.write('\n')
+        fw.write(f'{work_date.strftime("%Y-%m-%d")};{len(list)}')
+        work_date = work_date + timedelta(hours=24)
+
+    fw.close()
+
+
+def main():
+    ##for testing
+    if args.mode == 'TEST':
+        make_test()
+
+    ##Getting arguemnts
     import os
-
-    parser = argparse.ArgumentParser(description='NASA Get Scenes. Patch to retrieve DT scenes')
-    parser.add_argument("-m", "--mode", help="Mode", choices=["LIST", "DOWNLOAD", "NRT_TO_DT","TEST"])
-    parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
-    parser.add_argument("-sen", "--sensor", help="Specify sensor: VIIRS, VIIRSJ, AQUA, PACE_AOP", required=True)
-    parser.add_argument("-d", "--date",
-                        help="Specify a date in yyyymmdd format, or a date list file if option -list_dates is activated",
-                        required=True)
-    parser.add_argument("-R", "--Region", help="specify the Region Label of the area: BlackSea or Mediterranean",
-                        required=False)
-    parser.add_argument("-lat_p", "--lat_point", help="Station latitude")
-    parser.add_argument("-lon_p", "--lon_point", help="Stationg longitude")
-    parser.add_argument("-site", "--site_name", help="Pre-defined sites: VEIT,TRIT,LAIT,GATI")
-    parser.add_argument("-ed", "--end_date",
-                        help="End date for DOWNLOAD option in yyyymmdd format (=date if this option is ignored")
-    parser.add_argument("-list_dates", "--use_list_dates",
-                        help="If this option is given, -d shoud be a text file containing a data list in format yyyy-mm-dd",
-                        action="store_true")
-    parser.add_argument("-nrt","--use_nrt",help="Download/list NRT files",action="store_true")
-    parser.add_argument("-o", "--path_out", help="Path out")
-    parser.add_argument("-ow", "--overwrite", help="Overwrite download files")
-    args = parser.parse_args()
-
     from nasa_download import NASA_DOWNLOAD
     from datetime import datetime as dt
-
+    ndownload = NASA_DOWNLOAD()
+    site_name = None
+    lat_point = None
+    lon_point = None
+    date_here = None
     try:
         if args.use_list_dates:
             file_list_dates = args.date
             if not os.path.exists(file_list_dates):
                 print(f'[ERROR] File {file_list_dates} is not available')
-                exit(0)
+                return
         else:
             date_here = dt.strptime(args.date, '%Y%m%d')
-        sensor = args.sensor
-        region = None
-        lat_point = None
-        lon_point = None
-        if args.Region:
-            if args.Region == 'BlackSea':
-                region = 'BS'
-            elif args.Region == 'Mediterranean':
-                region = 'MED'
-            elif args.Region == 'BalticSea':
-                region = 'BAL'
-        if args.lat_point:
+        sensor = ndownload.check_sensor(args.sensor)
+        if sensor is None:
+            return
+        if args.lat_point and args.lon_point:
             lat_point = float(args.lat_point)
-        if args.lon_point:
             lon_point = float(args.lon_point)
-        if args.site_name:
-            lat_point, lon_point = get_lat_lon_from_site_name(args.site_name)
-            if lat_point is None and lon_point is None:
-                exit(1)
-        # date_here = dt(2022,9,30)
+        else:
+            if args.site_name:
+                site_name = ndownload.check_site(args.site_name)
+                if site_name is None:
+                    return
+            else:
+                print(
+                    f'[ERROR] Geographic information is not provided. Please use -site (--site_name) or provide point coordinates using -lat_p(--lat_point) and -lon_p(--lon_point) options')
+                return
     except:
         print('[ERROR] Error parsing arguments')
-        exit(1)
-
-
-    if args.mode == 'TEST':
-        from datetime import datetime as dt
-        from datetime import timedelta
-        fout = '/mnt/c/DATA_LUIS/OCTAC_WORK/BAL_EVOLUTION_202411/AQUA_2019.csv'
-        fw = open(fout,'w')
-        work_date = dt(2019,6,1)
-        end_date = dt(2019,9,30)
-        geo_limits = [53, 65, 12, 31]
-        ndownload = NASA_DOWNLOAD()
-        fw.write('Date;NImages')
-        while work_date<=end_date:
-            print('DATE: ',work_date)
-            #work_date_str = work_date.strftime('%Y%m%d')
-            list = ndownload.getscenes_by_region_EarthData_API('AQUA', work_date, geo_limits, True)
-            fw.write('\n')
-            fw.write(f'{work_date.strftime("%Y-%m-%d")};{len(list)}')
-            work_date = work_date + timedelta(hours=24)
-
-        fw.close()
+        return
 
     if args.mode == 'LIST':
-        ndownload = NASA_DOWNLOAD()
+        if args.use_list_dates:
+            print(f'[WARNING] LIST mode is not allowed with -list_date (--use_list_dates) argument')
+            return
+        is_dt = True if not args.use_nrt else False
         list = None
-        if region is not None:
-            if region=='BAL':
-                geo_limits = [53,65,12,31]
-                list = ndownload.getscenes_by_region_EarthData_API(sensor,date_here,geo_limits,True)
-            else:
-                list = ndownload.get_list_files(date_here, sensor, region, 'DT')
+        if site_name is not None:
+            list = ndownload.getscenes_by_site_EarthData_API(sensor, date_here, site_name, is_dt)
         elif lat_point is not None and lon_point is not None:
-            is_dt = True if not args.use_nrt else False
-            list = ndownload.getscenes_by_point_EarthData_API(sensor, date_here, lat_point, lon_point,is_dt)
-
+            list = ndownload.getscenes_by_point_EarthData_API(sensor, date_here, lat_point, lon_point, is_dt)
         if list is not None:
             for granule in list:
                 print(granule)
 
-
-
     if args.mode == 'DOWNLOAD':
-        ndownload = NASA_DOWNLOAD()
-        path_out = None
         if args.path_out and os.path.isdir(args.path_out):
             ndownload.sensors[sensor]['nrt_cnr_server_dir'] = args.path_out
 
         if not args.use_list_dates:
+            if date_here is None:
+                return
             end_date = date_here
             if args.end_date:
                 end_date = dt.strptime(args.end_date, '%Y%m%d')
-        import obdaac_download as od
+
         import os
         from datetime import timedelta
 
-        appkey = '22da4a89034645c3653f75dcd49ea11639976cef'
+        #appkey = '22da4a89034645c3653f75dcd49ea11639976cef'
 
         if args.use_list_dates:
             fdates = open(file_list_dates, 'r')
             for line in fdates:
                 try:
                     date_here = dt.strptime(line.strip(), '%Y-%m-%d')
-                    download_date(ndownload, sensor, region, lat_point, lon_point, date_here)
+                    download_date(ndownload, sensor, site_name, lat_point, lon_point, date_here)
                 except:
                     print(f'[WARNING] Date {line.strip()} is not in the correct format. Skipping...')
             fdates.close()
-            exit(0)
+            return
 
         while date_here <= end_date:
-            download_date(ndownload, sensor, region, lat_point, lon_point, date_here)
+            download_date(ndownload, sensor, site_name, lat_point, lon_point, date_here)
 
             date_here = date_here + timedelta(hours=24)
 
     if args.mode == 'NRT_TO_DT':
-        ndownload = NASA_DOWNLOAD()
-        list_nasa = ndownload.get_list_files(date_here, sensor, region, 'DT')
+        list_nasa = ndownload.get_list_files(date_here, sensor, site_name, 'DT')
         list_nasa = [l.strip() for l in list_nasa]
         list_dest = ndownload.get_list_files_orig(sensor, date_here)
         print(list_dest)
-
         # check if files are in the ftp
         for name_orig in list_dest:
             name = list_dest[name_orig]
@@ -225,11 +210,9 @@ if __name__ == '__main__':
                 else:
                     print('WARNING', name, '->', namen)
                     list_dest[name_orig] = 'NaN'
-
         # download
         import obdaac_download as od
         import os
-
         appkey = '22da4a89034645c3653f75dcd49ea11639976cef'
         for name_orig in list_dest:
             name = list_dest[name_orig]
@@ -247,3 +230,7 @@ if __name__ == '__main__':
                 file_orig_copy = os.path.join(nrtfolder, name_orig)
                 os.rename(file_orig, file_orig_copy)
                 os.rename(file_dest, file_orig)
+
+
+if __name__ == '__main__':
+    main()
--- nasa_download.py
+++ nasa_download.py
@@ -12,6 +12,25 @@ import json
 
 class NASA_DOWNLOAD:
     def __init__(self):
+        import __init__
+        main_path = os.path.dirname(__init__.__file__)
+        config_file = os.path.join(main_path,'config_download_tool.ini')
+        if not os.path.exists(config_file):
+            config_file = os.path.join(os.path.dirname(main_path),'config_download_tool.ini')
+        if not os.path.exists(config_file):
+            print('[WARNING] Configuration file config_download_tool does not exist. Some charteristics could not be loaded')
+
+        self.apikey = '22da4a89034645c3653f75dcd49ea11639976cef'
+        self.apikey_list = {}
+        self.sensors_info = {}
+        self.sensors_names = {}
+        self.sites_info = {}
+        self.sites_names = {}
+        if os.path.exists(config_file):
+            self.load_configuration(config_file)
+
+
+
         self.direct_access_base_url = 'https://oceandata.sci.gsfc.nasa.gov/directdataaccess/Level-2/'
         self.url_download = 'https://oceandata.sci.gsfc.nasa.gov/ob/getfile/'
         self.api_download = 'https://cmr.earthdata.nasa.gov/search/granules.umm_json?page_size=20&sort_key=short_name&sort_key=start_date'
@@ -68,6 +87,82 @@ class NASA_DOWNLOAD:
             }
         }
 
+    def load_configuration(self,fconfig):
+        import configparser
+        options = configparser.ConfigParser()
+        options.read(fconfig)
+        for section in options.sections():
+            if section=='nasa_credentials':
+                for opt in options[section].keys():
+                    self.apikey_list[opt] = options[section][opt].strip()
+                if 'default' in self.apikey_list:
+                    self.apikey = self.apikey_list['default']
+            else:
+                if not 'type' in options[section].keys():
+                    continue
+                if options[section]['type']=='sensor':
+                    self.load_sensor(section.upper(),options[section])
+                if options[section]['type']=='site':
+                    self.load_site(section.upper(),options[section])
+
+        # print(self.apikey_list)
+        # print(self.apikey)
+        # print(self.sensors_info)
+        # print(self.sensors_names)
+
+    def load_site(self,site_name,site_section):
+        self.sites_info[site_name]={
+            'site_type': 'area',
+            'geo': None,
+        }
+        self.sites_names[site_name] = site_name
+        if 'alt_names' in site_section.keys():
+            alt_names = site_section['alt_names'].strip()
+            for x in alt_names.split(','):
+                self.sites_names[x] = site_name
+
+        for opt in site_section.keys():
+            if opt=='geo':
+                try:
+                    geo = [float(x) for x in site_section[opt].strip().split(',')]
+                    if len(geo)==2 or len(geo)==4:
+                        self.sites_info[site_name]['geo'] = geo
+                        if len(geo)==2:
+                            self.sites_info[site_name]['site_type'] = 'point'
+                except:
+                    pass
+            else:
+                self.sites_info[site_name][opt] = site_section[opt]
+
+    def load_sensor(self,sensor_name,sensor_section):
+        self.sensors_info[sensor_name]={}
+        self.sensors_names[sensor_name] = sensor_name
+        if 'alt_names' in sensor_section.keys():
+            alt_names = sensor_section['alt_names'].strip()
+            for x in alt_names.split(','):
+                self.sensors_names[x] = sensor_name
+
+        for opt in sensor_section.keys():
+            self.sensors_info[sensor_name][opt] = sensor_section[opt]
+
+    def check_sensor(self,s):
+        if s.upper() not in self.sensors_names.keys():
+            print(f'[ERROR] Sensor {s} is not available in the configuration file')
+            return None
+        return self.sensors_names[s.upper()]
+
+    def check_site(self,s):
+        if s.upper() not in self.sites_names:
+            print(f'[ERROR] Site {s} is not available in the configuration file')
+            return None
+
+        site_ref = self.sites_names[s.upper()]
+
+        if self.sites_info[site_ref]['geo'] is None:
+            print(f'[ERROR] Geo information for site {s} is not valid')
+            return None
+        return site_ref
+
     def check_scenes_med_bs_API(self,sen,date_here):
         ##check first dt
         short_name = self.sensors[sen]['short_name']
@@ -118,6 +213,8 @@ class NASA_DOWNLOAD:
         granules = []
         if ngranules > 0:
             for item in data['items']:
+                print('---------------------------------------------------------')
+                print(item)
                 name = item['umm']['DataGranule']['ArchiveAndDistributionInformation'][0]['Name']
                 date_name = dt.strptime(name.split('.')[1], '%Y%m%dT%H%M%S')
                 if self.time_min <= date_name.hour <= self.time_max:
@@ -128,8 +225,6 @@ class NASA_DOWNLOAD:
 
         short_name = self.sensors[sen]['short_name']
         if is_dt: short_name = short_name.replace('_NRT', '')
-
-
 
         lon_min = insitu_lon - 0.5
         lon_max = insitu_lon + 0.5
@@ -153,6 +248,25 @@ class NASA_DOWNLOAD:
                 if self.time_min <= date_name.hour <= self.time_max:
                     granules.append(item['umm']['DataGranule']['ArchiveAndDistributionInformation'][0]['Name'])
         return granules
+
+    def getscenes_by_site_EarthData_API(self,sensor,date,site,is_dt):
+        if 'time_min' in self.sites_info[site].keys() and 'time_max' in self.sites_info[site].keys():
+            try:
+                tmin = int(self.sites_info[site]['time_min'].strip())
+                tmax = int(self.sites_info[site]['time_max'].strip())
+                self.time_min = tmin
+                self.time_max = tmax
+            except:
+                print(f'[WARNING] time_mix and time_max options for site {site} are not valid. Default options (6-18) will be used')
+        geo_info = self.sites_info[site]['geo']
+        site_type = self.sites_info[site]['site_type']
+        list = None
+        if site_type=='point':
+            list = self.getscenes_by_point_EarthData_API(sensor,date,geo_info[0],geo_info[1],is_dt)
+        elif site_type=='area':
+            list = self.getscenes_by_region_EarthData_API(sensor,date,geo_info,is_dt)
+        return list
+
 
     def get_path_orig(self, sensor, date_here):
         path = self.sensors[sensor]['nrt_cnr_server_dir']
